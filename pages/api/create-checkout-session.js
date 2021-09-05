6190129968f6869aa93bfe783b0493f17631551f
The-Indian-Programmer <85428889+The-Indian-Programmer@users.noreply.github.com>
2021-09-05 10:28:23 +0530

    stripe handle error

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,4 +1,5 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+stripe.setMaxNetworkRetries(2);
 const checkoutfunction = async (req, res) => {
   const { items, email } = req.body;
   let imageArr = [];
